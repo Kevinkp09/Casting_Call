2f2b410be9a49ffff07d70d7e3b3b2dbe0ed839d
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-03-12 19:02:49 +0530

    Implementing login functionality

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-    skip_before_action :doorkeeper_authorize!, only: %i[create]
+    skip_before_action :doorkeeper_authorize!, only: %i[create login]
 
     def create
       user = User.new(user_params)
@@ -33,6 +33,35 @@ class Api::V1::UsersController < ApplicationController
         })
       else
         render(json: { error: user.errors.full_messages }, status: 422)
+      end
+    end
+
+    def login
+      user = User.find_for_authentication(email: params[:user][:email])
+      if user&.valid_password?(params[:user][:password])
+
+        client_app = Doorkeeper::Application.find_by(uid: params[:client_id])
+        access_token = Doorkeeper::AccessToken.create(
+          resource_owner_id: user.id,
+          application_id: client_app.id,
+          refresh_token: generate_refresh_token,
+          expires_in: Doorkeeper.configuration.access_token_expires_in.to_i,
+          scopes: ''
+        )
+
+        render(json: {
+          user: {
+            id: user.id,
+            email: user.email,
+            access_token: access_token.token,
+            token_type: 'bearer',
+            expires_in: access_token.expires_in,
+            refresh_token: access_token.refresh_token,
+            created_at: access_token.created_at.to_time.to_i
+          }
+        })
+      else
+        render json: {error: "Invalid email or password"}, status: :unprocessable_entity
       end
     end
 
