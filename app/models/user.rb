fc712cedc75998405d81b3927f4c81f537daf10a
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-03-22 11:48:53 +0530

    Changing enum from array to hash, removed unnecessary code and changing before_create action to after create in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  enum role: [:artist, :agency, :admin] 
+  enum role: {artist: 0, agency: 1, admin: 3}
          VALID_MOBILE_REGEX = /\A\d{10}\z/
          VALID_LINK_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/
   has_one_attached :profile_photo
@@ -12,30 +12,15 @@ class User < ApplicationRecord
   validates :mobile_no, presence: true, format: { with: VALID_MOBILE_REGEX, message: "Invalid" }
   validates :username, presence: true
   validates :role, presence: true
-  before_create :generate_otp
+  after_create :generate_otp
 
    def generate_otp
       self.otp = rand.to_s[2..5]
       UserMailer.send_otp_email(self).deliver_now
    end
 
-  def self.authenticate!(email, credential, credential_type)
-    user = find_by(email: email)
-    if user && user.valid_credential?(credential, credential_type)
-      user
-    else
-      nil
-    end
-  end
-
-  def valid_credential?(credential, credential_type)
-    case credential_type.to_sym
-    when :password
-      valid_password?(credential)
-    when :otp
-      self.otp == credential ? true : false
-    else
-      false
-    end
-  end
+   def self.authenticate(email, password)
+      user = User.find_for_authentication(email: email)
+      user&.valid_password?(password) ? user : nil
+   end
 end
