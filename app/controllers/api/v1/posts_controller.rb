41744dcce5aa7bd1aeca62e9d23532e90f6cef51
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-04-02 11:02:23 +0530

    Implementing index action for showing all the posts the agency has posted.

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,21 +1,19 @@
 class Api::V1::PostsController < ApplicationController
   before_action :approved_agency
-  def create
-    user = current_user
-    if user.role == "agency" && user.approval_status == "approved"
-      post = user.posts.new(post_params)
-      if post.save
-        render json: {message: "Post added successfully"}, status: :ok
-      else
-        render json: {error: "Error in creating post"}, status: :unprocessable_entity
-      end
-    else
-      render json: {error: "You are not authorized for this action"}, status: :unprocessable_entity
-    end
+
+  def index
+    posts = user.posts
+    render json: posts, status: :ok
   end
 
-  def view_applied_artist
-    
+  def create
+    user = current_user
+    post = user.posts.new(post_params)
+    if post.save
+      render json: {message: "Post added successfully"}, status: :ok
+    else
+      render json: {error: "Error in creating post"}, status: :unprocessable_entity
+    end
   end
 
   private
@@ -24,6 +22,6 @@ class Api::V1::PostsController < ApplicationController
   end
 
   def approved_agency
-    user.approval_status = approved
+    user.role == "agency" && user.approval_status = approved
   end
 end
