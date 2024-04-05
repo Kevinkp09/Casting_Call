847d8026487ba2c55723c2c89bdfadb1cb020c9f
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-04-05 11:20:47 +0530

    Refactoring the code and adding private methods for separating the repititive code used.

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::RequestsController < ApplicationController
+  before_action :set_post, only: [:index, :create]
+  before_action :check_request_status, only: [:approve_artist, :reject_artist]
   def index
-    post = Post.find(params[:post_id])
-    requests = post.requests
+    requests = @post.requests
     render json: requests, status: :ok
   end
 
   def create
     if current_user.role == "artist"
-      post = Post.find(params[:post_id])
-      request = post.requests.new(user: current_user)
+      request = @post.requests.new(user: current_user)
       if request.save
         render json: request, status: :created
       else
@@ -20,24 +20,16 @@ class Api::V1::RequestsController < ApplicationController
   end
 
   def approve_artist
-    request = Request.find(params[:id])
-    if request.status == "approved" || request.status == "rejected"
-      render json: {error: "Request is already approved or rejected, it can't be done again"}, status: :unprocessable_entity
-    end
-    if request.update(status: :approved)
-      render json: {message: "Request is approved", id: request.id, status: request.status}, status: :ok
+    if @request.update(status: :approved)
+      render json: {message: "Request is approved", id: @request.id, status: @request.status}, status: :ok
     else
       render json: {error: request.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
   def reject_artist
-    request = Request.find(params[:id])
-    if request.status == "approved" || request.status == "rejected"
-      render json: {error: "Request is already approved or rejected, it can't be done again"}, status: :unprocessable_entity
-    end
-    if request.update(status: :rejected)
-      render json: {message: "Request is rejected", id: request.id, status: request.status}, status: :ok
+    if @request.update(status: :rejected)
+      render json: {message: "Request is rejected", id: @request.id, status: @request.status}, status: :ok
     else
       render json: {error: request.errors.full_messages}, status: :unprocessable_entity
     end
@@ -46,5 +38,20 @@ class Api::V1::RequestsController < ApplicationController
   private
   def request_params
     params.require(:request).permit(:status)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def check_request_status
+    @request = Request.find(params[:id])
+    if @request.nil?
+      render json: {message: "Request not found"}, status: 404
+      return
+    end
+    if @request.status == "approved" || @request.status == "rejected"
+      render json: {error: "Request is already approved or rejected, it can't be done again"}, status: :unprocessable_entity
+    end
   end
 end
