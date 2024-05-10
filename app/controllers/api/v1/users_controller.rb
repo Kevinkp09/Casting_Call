3c641021665df107a07a5e5cc6935a6474df60ad
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-05-10 15:02:59 +0530

    Sending gender and birth date while registering a new user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -29,6 +29,8 @@ class Api::V1::UsersController < ApplicationController
           email: user.email,
           access_token: access_token.token,
           token_type: 'bearer',
+          gender: user.gender,
+          birth_date: user.birth_date,
           expires_in: access_token.expires_in,
           refresh_token: access_token.refresh_token,
           created_at: access_token.created_at.to_time.to_i
