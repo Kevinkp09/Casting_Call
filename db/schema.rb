12d58a8d86664cec01e2972072fdfec6037f87bc
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-06-03 15:50:02 +0530

    Changes made to payments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_30_092010) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_03_101650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,6 +77,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_30_092010) do
     t.datetime "updated_at", null: false
     t.integer "posts_limit"
     t.integer "requests_limit"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.integer "agency_id"
+    t.integer "package_id"
+    t.string "razorpay_order_id"
+    t.string "razorpay_payment_id"
+    t.string "status_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
