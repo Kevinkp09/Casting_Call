4b51e1109f073560c67c59b98cc0c33ed5e3db9c
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-06-11 14:18:10 +0530

    Removing unnecessary lines of code.

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,5 +3,4 @@ class Request < ApplicationRecord
   belongs_to :post
   enum status: {waiting: 0, shortlisted: 1, rejected: 2}
   enum apply_status: {apply: 0, applied: 1}
-  VALID_LINK_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/
 end
